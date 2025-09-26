3469553df33346ef91e7989cc3d5ee88fdee6586
nowwith <90362794+julie8291@users.noreply.github.com>
2025-09-26 15:08:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // !!! 중요: README.md 파일을 읽고, 배포된 자신의 Google Apps Script 웹 앱 URL로 변경하세요.
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwkmo2oqqgQmG7JrrK_Nc2rtk8uHaASJLpwvxVmUVf6s00n6cx4DaUZPa97j4Oj6nKQ/exec';
+    const WEB_APP_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
 
     const recordForm = document.getElementById('record-form');
     const recordsContainer = document.getElementById('records-container');
